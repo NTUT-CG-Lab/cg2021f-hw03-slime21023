bb7f907c839a4681a06fb5a004fb1d5a9b31e9a3
slime21023 <slime21023@gmail.com>
2021-11-05 16:09:06 +0800

    update

--- main.js
+++ main.js
@@ -6,90 +6,139 @@ import {
     DirectionalLight,
     Vector3,
     Box3,
-    TetrahedronGeometry
 } from './build/three.module.js'
-// import { GUI } from './jsm/libs/dat.gui.module.js'
-import { OrbitControls } from './jsm/controls/OrbitControls.js'
 import { OutlineEffect } from './jsm/effects/OutlineEffect.js'
 import { MMDLoader } from './jsm/loaders/MMDLoader.js'
-// import { MMDAnimationHelper } from './jsm/animation/MMDAnimationHelper.js'
 
-const scene = new Scene()
-const renderer = new WebGLRenderer({ antialias: true })
-renderer.autoClear = false
-
- // TODO
 let SCREEN_WIDTH = window.innerWidth
 let SCREEN_HEIGHT = window.innerHeight
 let aspect = SCREEN_WIDTH / SCREEN_HEIGHT
 const frustumSize = 30
+const container = document.createElement('div')
+document.body.appendChild(container)
+
 const modelList = [
     'models/mmd/kizunaai/kizunaai.pmx',
     // 'models/mmd/るいのれ式物述有栖_配布用フォルダ/物述有栖.pmx',
     'models/mmd/『天宮こころ(Kokoro Amamiya)』/『天宮こころ(Kokoro Amamiya)』.pmx'
 ]
 
-let modelIndex = 1
+const onProgress = xhr => {
+    if (xhr.lengthComputable) {
+        const percentComplete = xhr.loaded / xhr.total * 100
+        console.log(`${Math.round(percentComplete, 2)}% downloaded`)
+    }
+}
 
-Ammo().then(AmmoLib => {
-    Ammo = AmmoLib
-    init()
-    animate()
+const loadModel = url => new Promise(resolve => {
+    const loader = new MMDLoader()
+    loader.load(url, model => {
+        model.position.y = -10
+        resolve(model)
+    }, onProgress, null)
 })
 
+let reviser
 const animate = () => {
     requestAnimationFrame(animate)
     reviser.render()
 }
 
-class IrisReviser {
-    constructor(renderer) {
-        renderer.setPixelRatio(window.devicePixelRatio)
-        renderer.setSize(window.innerWidth, window.innerHeight)
-        this.cameras = [
-            new OrthographicCamera(0.5 * frustumSize * aspect / - 2, 0.5 * frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 0.1, 1000),
-            new OrthographicCamera(0.25 * frustumSize * aspect / - 2, 0.25 * frustumSize * aspect / 2, 0.5 * frustumSize / 2, 0.5 * frustumSize / - 2, 0.1, 1000),
-            new OrthographicCamera(0.25 * frustumSize * aspect / - 2, 0.25 * frustumSize * aspect / 2, 0.5 * frustumSize / 2, 0.5 * frustumSize / - 2, 0.1, 1000),
-            new OrthographicCamera(0.25 * frustumSize * aspect / - 2, 0.25 * frustumSize * aspect / 2, 0.5 * frustumSize / 2, 0.5 * frustumSize / - 2, 0.1, 1000),
-            new OrthographicCamera(0.25 * frustumSize * aspect / - 2, 0.25 * frustumSize * aspect / 2, 0.5 * frustumSize / 2, 0.5 * frustumSize / - 2, 0.1, 1000)
-        ]
+Promise.all(modelList.map(loadModel)).then(models => {
+    reviser = new IrisReviser(models)
+    // console.log(models)
+    Ammo().then(AmmoLib => {
+        Ammo = AmmoLib
+        animate()
+    })
+})
 
-        this.cameras.forEach(camera => {
-            camera.position.z = 30
-            scene.add(camera)
+const createResource = () => {
+    const renderer = new WebGLRenderer({ antialias: true })
+    renderer.autoClear = false
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+
+    const scene = new Scene()
+    const ambient = new AmbientLight(0x666666)
+    ambient.layers.enableAll()
+    scene.add(ambient)
+
+    const directionalLight = new DirectionalLight(0x887766)
+    directionalLight.layers.enableAll()
+    directionalLight.position.set(- 1, 1, 1).normalize()
+    scene.add(directionalLight)
+
+    const effect = new OutlineEffect(renderer)
+    const cameras = [
+        new OrthographicCamera(0.5 * frustumSize * aspect / - 2, 0.5 * frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 0.1, 1000),
+        new OrthographicCamera(0.25 * frustumSize * aspect / - 2, 0.25 * frustumSize * aspect / 2, 0.5 * frustumSize / 2, 0.5 * frustumSize / - 2, 0.1, 1000),
+        new OrthographicCamera(0.25 * frustumSize * aspect / - 2, 0.25 * frustumSize * aspect / 2, 0.5 * frustumSize / 2, 0.5 * frustumSize / - 2, 0.1, 1000),
+        new OrthographicCamera(0.25 * frustumSize * aspect / - 2, 0.25 * frustumSize * aspect / 2, 0.5 * frustumSize / 2, 0.5 * frustumSize / - 2, 0.1, 1000),
+        new OrthographicCamera(0.25 * frustumSize * aspect / - 2, 0.25 * frustumSize * aspect / 2, 0.5 * frustumSize / 2, 0.5 * frustumSize / - 2, 0.1, 1000)
+    ]
+    cameras.forEach(cam => {
+        cam.position.z = 30
+        scene.add(cam)
+    })
+
+    return { renderer, scene, effect, cameras }
+}
+
+class IrisReviser {
+    constructor(models) {
+        this.models = models
+        this.modelIndex = 0
+        const resources = models.map(model => {
+            const { renderer, scene, effect, cameras } = createResource()
+            const meshes = [model.clone(), model.clone(), model.clone(), model.clone()]
+            meshes.forEach((mesh, index) => {
+                mesh.layers.set(index + 1)
+                scene.add(mesh)
+            })
+
+            cameras.forEach((cam, index) => {
+                if (index == 0) {
+                    cam.layers.set(1)
+                    return
+                }
+
+                cam.layers.set(index)
+                cam.updateProjectionMatrix()
+            })
+            return { renderer, scene, effect, cameras, meshes }
         })
-        this.effect = new OutlineEffect(renderer)
-        // this.control = new OrbitControls(this.cameras[0], renderer.domElement)
-        // this.control.minDistance = 10
-        // this.control.maxDistance = 100
-        // this.control.enableRotate = false
+        this.res = resources
+        const target = this.res[this.modelIndex]
+        container.appendChild(target.renderer.domElement)
+        window._scene = target.scene
     }
 
     render() {
-        const cameras = this.cameras
-        const effect = this.effect
+        const target = this.res[this.modelIndex]
+        const cameras = target.cameras
+        const effect = target.effect
+        const scene = target.scene
+
         effect.clear()
         effect.setViewport(0, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT)
-        effect.render(scene, this.cameras[0])
-
+        effect.render(scene, cameras[0])
         effect.setViewport(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2)
         effect.render(scene, cameras[1])
-
         effect.setViewport(SCREEN_WIDTH / 2 + SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2)
         effect.render(scene, cameras[2])
-
         effect.setViewport(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2)
         effect.render(scene, cameras[3])
-
         effect.setViewport(SCREEN_WIDTH / 2 + SCREEN_WIDTH / 4, 0, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2)
         effect.render(scene, cameras[4])
     }
 
     onResize() {
         const aspect = window.innerWidth / window.innerHeight
-        this.effect.setSize(window.innerWidth, window.innerHeight)
-        const cameras = this.cameras
-
+        const target = this.res[this.modelIndex]
+        const effect = target.effect
+        effect.setSize(window.innerWidth, window.innerHeight)
+        const cameras = target.cameras
         cameras[0].left = 0.5 * frustumSize * aspect / -2
         cameras[0].right = 0.5 * frustumSize * aspect / 2
         cameras[0].top = frustumSize / 2
@@ -121,127 +170,34 @@ class IrisReviser {
         cameras[4].updateProjectionMatrix()
     }
 
-    setCameraToTarget(meshes) {
-        
-        const first = meshes[0]
-        first.geometry.computeBoundingBox()
-        const box = new Box3()
-        box.copy(first.geometry.boundingBox)
-        const width = box.max.x - box.min.x
-        const height = box.max.y - box.min.y
-        console.log(width, height)
+    preModel() {
+        if (this.modelIndex == 0) return
+        this.clearContainer()
 
-        const face = first.skeleton.bones[8]
-        console.log(face)
-        const faceCenter = new Vector3()
-        faceCenter.setFromMatrixPosition(face.matrixWorld)
-        
-        // face.getWorldPosition(faceCenter)
-
-        this.cameras.forEach(cam => {
-            cam.position.set(faceCenter.x, faceCenter.y, 20)
-            cam.zoom = 1.2
-            cam.near = 15
-            cam.lookAt(faceCenter)
-            cam.updateProjectionMatrix()
-        })
-        
-        // console.log(faceCenter)
-        // const eye = model.skeleton.bones[88]
-        // eye.getWorldPosition(this.cameras[0].position)
-        // this.cameras[0].position.setZ(1)
-        // this.controls[0].minZoom = 15
-        // eye.getWorldPosition(this.controls[0].target)
-        // this.controls[0].update()
+        const target = this.res[--this.modelIndex]
+        container.appendChild(target.renderer.domElement)
     }
 
-    async setModel(url) {
-        console.log(renderer.state)
-        this.effect.clear()
-        console.log(renderer.state)
-        const loader = new MMDLoader()
-        const onProgress = xhr => {
-            if (xhr.lengthComputable) {
-                const percentComplete = xhr.loaded / xhr.total * 100
-                console.log(`${Math.round(percentComplete, 2)}% downloaded`)
-            }
-        }
+    nextModel() {
+        if (this.modelIndex == this.res.length - 1) return
+        this.clearContainer()
 
-        const loadModel = url => new Promise(resolve => {
-            loader.load(url, model => {
-                model.position.y = -10
-                resolve(model)
-            }, onProgress, null)
-        })
-
-        const model = await loadModel(url)
-        this.meshes = [model.clone(), model.clone(), model.clone(), model.clone()]
-        this.meshes.forEach((mesh, index) => {
-            mesh.layers.set(index + 1)
-            scene.add(mesh)
-        })
-
-        this.cameras.forEach((cam, index) => {
-            if (index == 0) {
-                cam.layers.toggle(1)
-                return
-            }
-            cam.layers.toggle(index)
-            cam.updateProjectionMatrix()
-        })
-        this.setCameraToTarget(this.meshes)
-        this.cameras.forEach(cam => {
-            renderer.render(scene, cam)
-        })
-
+        const target = this.res[++this.modelIndex]
+        container.appendChild(target.renderer.domElement)
     }
 
-    async cleanMeshes() {
-        this.meshes.forEach(mesh => {
-            scene.remove(mesh)
-            mesh.geometry.dispose()
-        })
-        delete this.meshes
+    clearContainer() {
+        container.innerHTML = ''
     }
-}
-
-const reviser = new IrisReviser(renderer)
-
-const init = async () => {
-    const container = document.createElement('div')
-    document.body.appendChild(container)
-
-    const ambient = new AmbientLight(0x666666)
-    ambient.layers.enableAll()
-    scene.add(ambient)
-
-    const directionalLight = new DirectionalLight(0x887766)
-    directionalLight.layers.enableAll()
-    directionalLight.position.set(- 1, 1, 1).normalize()
-    scene.add(directionalLight)
-    container.appendChild(renderer.domElement)
-
-    window._scene = scene
-    reviser.setModel(modelList[modelIndex])
 }
 
 window.addEventListener('resize', () => { reviser.onResize() })
-
-document.addEventListener('keydown', async ({ key }) => {
+document.addEventListener('keydown', ({ key }) => {
     if (key == 'a' || key == 'A') {
-        if (modelIndex == 0) return
-        reviser.cleanMeshes().then(() => {
-            modelIndex -= 1
-            reviser.setModel(modelList[modelIndex])
-        })
+        reviser.preModel()
     }
 
     if (key == 'd' || key == 'D') {
-        if (modelIndex == modelList.length - 1) return
-        reviser.cleanMeshes().then(() => {
-            modelIndex += 1
-            reviser.setModel(modelList[modelIndex])
-        })
+        reviser.nextModel()
     }
-
-}, false)
+})
